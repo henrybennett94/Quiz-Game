8f66684bda28c224dd7649a6a3a937da9f9cecd6
Craig Allen <callen7@hotmail.co.uk>
2024-07-03 10:10:30 +0000

    added more questions for each room

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,33 @@ var rooms = {
                 question: 'What spell is used to disarm an opponent?',
                 correctAnswer: 'Expelliarmus',
                 choices: ['Expelliarmus', 'Avada Kedavra', 'Alohomora', 'Wingardium Leviosa']
+            },
+            {
+                question: 'What is the name of the train that takes students to Hogwarts?',
+                correctAnswer: 'Hogwarts Express',
+                choices: ['Knight Bus', 'Hogwarts Express', 'Thestral Carriage', 'Nimbus 2000']
+            },
+            {
+                question: 'Who is the Half-Blood Prince?',
+                correctAnswer: 'Severus Snape',
+                choices: ['Harry Potter', 'Sirius Black', 'Severus Snape', 'Tom Riddle']
+            },
+            {
+                question: 'What is the name of Hermione Granger\'s cat?',
+                correctAnswer: 'Crookshanks',
+                choices: ['Fluffy', 'Crookshanks', 'Scabbers', 'Fang']
+            },
+            {
+                question: 'What is the name of the Weasley family\'s magical house?',
+                correctAnswer: 'The Burrow',
+                choices: ['The Shrieking Shack', 'The Burrow', 'Grimmauld Place', 'Number 12']
+            },
+            {
+                question: 'What is the primary ingredient in Polyjuice Potion?',
+                correctAnswer: 'A piece of the person you want to become',
+                choices: ['Boomslang Skin', 'A piece of the person you want to become', 'Dragon Blood', 'Mandrake Root']
             }
+            
         ],
         nextRoom: 'library',
         backgroundImage: 'assets/images/livingroom.jpg'
@@ -57,6 +83,31 @@ var rooms = {
                 question: 'Which of these books is not written by J.R.R. Tolkien?',
                 correctAnswer: 'The Chronicles of Narnia',
                 choices: ['The Hobbit', 'The Lord of the Rings', 'The Silmarillion', 'The Chronicles of Narnia']
+            },
+            {
+                question: 'Who wrote "To Kill a Mockingbird"?',
+                correctAnswer: 'Harper Lee',
+                choices: ['John Steinbeck', 'Harper Lee', 'F. Scott Fitzgerald', 'Mark Twain']
+            },
+            {
+                question: 'Which author wrote "The Catcher in the Rye"?',
+                correctAnswer: 'J.D. Salinger',
+                choices: ['J.D. Salinger', 'Ernest Hemingway', 'F. Scott Fitzgerald', 'John Steinbeck']
+            },
+            {
+                question: 'Who is the author of "The Great Gatsby"?',
+                correctAnswer: 'F. Scott Fitzgerald',
+                choices: ['F. Scott Fitzgerald', 'Ernest Hemingway', 'John Steinbeck', 'J.D. Salinger']
+            },
+            {
+                question: 'Which author wrote "War and Peace"?',
+                correctAnswer: 'Leo Tolstoy',
+                choices: ['Leo Tolstoy', 'Fyodor Dostoevsky', 'Anton Chekhov', 'Ivan Turgenev']
+            },
+            {
+                question: 'Who wrote "The Three Body Problem"?',
+                correctAnswer: 'Cixin Liu',
+                choices: ['Pablo Neruda', 'Haruki Murakami', 'Cixin Liu', 'Agustina Bazterica']
             }
         ],
         nextRoom: 'dining-hall',
@@ -65,29 +116,54 @@ var rooms = {
     'dining-hall': {
         questions: [
             {
-                question: 'What is the sum of the prime numbers less than 10?',
-                correctAnswer: '17',
-                choices: ['17', '18', '16', '19']
+                question: 'What is 90 multipled by 6?',
+                correctAnswer: '540',
+                choices: ['450', '540', '630', '180']
             },
             {
-                question: 'What is 8 + 9?',
+                question: 'What is 8 plus 9?',
                 correctAnswer: '17',
                 choices: ['15', '16', '17', '18']
             },
             {
-                question: 'What is 10 + 7?',
+                question: 'What is 10 plus 7?',
                 correctAnswer: '17',
                 choices: ['16', '17', '18', '19']
             },
             {
-                question: 'What is 20 - 3?',
+                question: 'What is 20 minus 3?',
                 correctAnswer: '17',
                 choices: ['17', '18', '16', '19']
             },
             {
-                question: 'What is 34 / 2?',
+                question: 'What is 34 divided by 2?',
                 correctAnswer: '17',
                 choices: ['16', '17', '18', '19']
+            },
+            {
+                question: 'What is 27 divided by 3?',
+                correctAnswer: '9',
+                choices: ['7', '9', '12', '6']
+            },
+            {
+                question: 'What is 4 multiplied by 5?',
+                correctAnswer: '20',
+                choices: ['15', '18', '20', '22']
+            },
+            {
+                question: 'What is 3 squared plus 4?',
+                correctAnswer: '13',
+                choices: ['7', '10', '13', '16']
+            },
+            {
+                question: 'What is the sum of all interior angles of a triangle?',
+                correctAnswer: '180 degrees',
+                choices: ['120 degrees', '150 degrees', '180 degrees', '210 degrees']
+            },
+            {
+                question: 'What is 15% of 240?',
+                correctAnswer: '36',
+                choices: ['24', '30', '36', '40']
             }
         ],
         nextRoom: 'gallery',
@@ -119,7 +195,33 @@ var rooms = {
                 question: 'Name the artist that painted The Creation of Adam"?',
                 correctAnswer: 'Michelangelo',
                 choices: ['Claude Monet', 'Michelangelo', 'Damien Hirst', 'Salvador Dali']
+            },
+            {
+                question: 'Who painted "Guernica", a famous anti-war mural?',
+                correctAnswer: 'Pablo Picasso',
+                choices: ['Salvador Dali', 'Pablo Picasso', 'Joan Miro', 'Frida Kahlo']
+            },
+            {
+                question: 'Which artist is known for his "Campbell\'s Soup Cans"?',
+                correctAnswer: 'Andy Warhol',
+                choices: ['Roy Lichtenstein', 'Jackson Pollock', 'Andy Warhol', 'Keith Haring']
+            },
+            {
+                question: 'Who created the sculpture "The Thinker"?',
+                correctAnswer: 'Auguste Rodin',
+                choices: ['Edgar Degas', 'Auguste Rodin', 'Constantin Brancusi', 'Henry Moore']
+            },
+            {
+                question: 'Which artist painted "Les Demoiselles d\'Avignon", a pioneering work of Cubism?',
+                correctAnswer: 'Pablo Picasso',
+                choices: ['Henri Matisse', 'Paul Cezanne', 'Pablo Picasso', 'Georges Braque']
+            },
+            {
+                question: 'Who painted "The Persistence of Memory", featuring melting clocks?',
+                correctAnswer: 'Salvador Dali',
+                choices: ['Rene Magritte', 'Salvador Dali', 'Joan Miro', 'Max Ernst']
             }
+            
         ],
         nextRoom: 'study',
         backgroundImage: 'assets/images/gallery.jpg'
@@ -150,6 +252,31 @@ var rooms = {
                 question: 'What is the pH level of pure water?',
                 correctAnswer: '7',
                 choices: ['7', '6', '5', '8']
+            },
+            {
+                question: 'Which particle is found in the nucleus of an atom?',
+                correctAnswer: 'Proton',
+                choices: ['Electron', 'Neutron', 'Proton', 'Photon']
+            },
+            {
+                question: 'What is the chemical formula for table salt?',
+                correctAnswer: 'NaCl',
+                choices: ['NaCl', 'H2O', 'CO2', 'CH4']
+            },
+            {
+                question: 'What is the hardest natural substance on Earth?',
+                correctAnswer: 'Diamond',
+                choices: ['Gold', 'Diamond', 'Graphite', 'Quartz']
+            },
+            {
+                question: 'What type of energy is stored in a battery?',
+                correctAnswer: 'Chemical energy',
+                choices: ['Electrical energy', 'Nuclear energy', 'Chemical energy', 'Solar energy']
+            },
+            {
+                question: 'What is the most abundant element in the universe?',
+                correctAnswer: 'Hydrogen',
+                choices: ['Oxygen', 'Carbon', 'Hydrogen', 'Helium']
             }
         ],
         nextRoom: 'observatory',
@@ -181,8 +308,33 @@ var rooms = {
                 question: 'Which planet is known as the Morning Star or Evening Star?',
                 correctAnswer: 'Venus',
                 choices: ['Venus', 'Mars', 'Mercury', 'Jupiter']
+            },
+            {
+                question: 'Which planet is nicknamed the "Blue Planet"?',
+                correctAnswer: 'Earth',
+                choices: ['Mercury', 'Venus', 'Earth', 'Mars']
+            },
+            {
+                question: 'Which planet has the Great Red Spot, a persistent anticyclonic storm?',
+                correctAnswer: 'Jupiter',
+                choices: ['Jupiter', 'Saturn', 'Uranus', 'Neptune']
+            },
+            {
+                question: 'Which planet has the highest mountain in the solar system, Olympus Mons?',
+                correctAnswer: 'Mars',
+                choices: ['Earth', 'Mars', 'Venus', 'Mercury']
+            },
+            {
+                question: 'Which planet has the most moons in the solar system?',
+                correctAnswer: 'Jupiter',
+                choices: ['Saturn', 'Jupiter', 'Uranus', 'Neptune']
+            },
+            {
+                question: 'Which planet is often referred to as the "Ice Giant"?',
+                correctAnswer: 'Uranus',
+                choices: ['Neptune', 'Uranus', 'Saturn', 'Jupiter']
             }
-        ],
+         ],
         nextRoom: 'kitchen',
         backgroundImage: 'assets/images/observatory.jpg'
     },
@@ -212,8 +364,33 @@ var rooms = {
                 question: 'Which nutrient is most abundant in eggs?',
                 correctAnswer: 'Protein',
                 choices: ['Carbohydrates', 'Protein', 'Fat', 'Vitamins']
+            },
+            {
+                question: 'What is the main ingredient in a traditional risotto?',
+                correctAnswer: 'Arborio rice',
+                choices: ['Spaghetti', 'Arborio rice', 'Quinoa', 'Barley']
+            },
+            {
+                question: 'Which spice is made from the dried stigma of a flower?',
+                correctAnswer: 'Saffron',
+                choices: ['Cinnamon', 'Saffron', 'Turmeric', 'Paprika']
+            },
+            {
+                question: 'Which fruit is known as the "king of fruits" and has a strong odor?',
+                correctAnswer: 'Durian',
+                choices: ['Mango', 'Pineapple', 'Durian', 'Lychee']
+            },
+            {
+                question: 'What is the primary ingredient in Worcestershire sauce?',
+                correctAnswer: 'Anchovies',
+                choices: ['Tomatoes', 'Anchovies', 'Vinegar', 'Onions']
+            },
+            {
+                question: 'Which alcoholic beverage is made from fermented honey and water?',
+                correctAnswer: 'Mead',
+                choices: ['Beer', 'Wine', 'Vodka', 'Mead']
             }
-        ],
+         ],
         nextRoom: 'final-room',
         backgroundImage: 'assets/images/kitchen.jpg'
     },
